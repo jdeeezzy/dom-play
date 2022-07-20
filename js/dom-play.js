68e40740128c9ecf3ea5590937e015bc170d0bec
Jdeeezzy <jdacquel@sea.yearup.com>
2022-07-20 11:35:56 -0700

    finish

--- js/dom-play.js
+++ js/dom-play.js
@@ -14,8 +14,33 @@ let spans = document.querySelectorAll("#play span");
 
 console.log(spans);
 
+// first for loop to set the event handler on the spans
 for(const mySpan of spans){
-    mySpan.addEventListener("click",myAlert);
+    mySpan.addEventListener("click",function (ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+    // alert(mySpan.dataset.actor);
+}
+
+// second for loop to distinguish the actors from each other 
+function highlightActor(actor){
+
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){// current actor, highlight
+            mySpan.style.backgroundColor='yellow';
+        }else{// not current actor, remove highlight
+            mySpan.style.backgroundColor="white";
+        }
+    }
+
+
+
+
+
+
+
+
+    // alert("The current actor is: " + actor);
 }
 
 function highlight(el){
